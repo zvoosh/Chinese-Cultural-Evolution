36d54cb92c55706835a05319e9ecddfa73f9e42e
Dusan Ilic <dusan.ilic1999@gmail.com>
2024-02-07 18:42:50 +0100

    fix:fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter >
+    <BrowserRouter basename='/chinese-cultural-evolution/' >
       <App />
     </BrowserRouter>
   </React.StrictMode>,
