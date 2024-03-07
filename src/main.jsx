a1452c106cdb142978ce37f719a137093743cd9d
Dusan Ilic <dusan.ilic1999@gmail.com>
2024-03-07 18:38:01 +0100

    fix: fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { HashRouter  } from 'react-router-dom'
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter  basename='/Chinese-Cultural-Evolution/' >
+    {/*  basename='/Chinese-Cultural-Evolution/' */}
+    <HashRouter>
       <App />
     </HashRouter >
   </React.StrictMode>,
-  document.getElementById('root')
 )
