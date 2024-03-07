a26aa354d3d2c4739dff95bf8eba5d7661626cab
Dusan Ilic <dusan.ilic1999@gmail.com>
2024-03-07 18:21:26 +0100

    fix: fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { WelcomePage, SculptingPage, FolklorePage, PaintingPage, PeoplePage, ChoosingPage} from './components';
 import NotFound from './components/NotFound';
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import './index.css'
 import { HashRouter  } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <HashRouter  basename='/Chinese-Cultural-Evolution/' >
       <App />
     </HashRouter >
   </React.StrictMode>,
+  document.getElementById('root')
 )
